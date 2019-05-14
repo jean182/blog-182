7704783f06a14861743bd2741d3de3d76f1728cf
Jean <jean-marco-10@hotmail.com>
2019-05-14 06:28:29 -0600

    Add bootstrap 4 grid

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,61 +1,89 @@
 import React, { Component } from "react"
 import addToMailchimp from "gatsby-plugin-mailchimp"
-import { IoMdMail } from "react-icons/io"
+import { IoMdMailOpen } from "react-icons/io"
 import { getSubscriptioPreferenceUrl } from "../utils/helpers"
 
 export default class Newsletter extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      name: '',
-      email: '',
+      name: "",
+      email: "",
       response: {},
-    };
+    }
   }
 
-  onNameChange = (e) => {
+  onNameChange = e => {
     const name = e.target.value
-    this.setState(() => ({ name }));
+    this.setState(() => ({ name }))
   }
 
-  onEmailChange = (e) => {
+  onEmailChange = e => {
     const email = e.target.value
-    this.setState(() => ({ email }));
+    this.setState(() => ({ email }))
   }
 
-  _handleSubmit = async (e) => {
-    e.preventDefault();
+  _handleSubmit = async e => {
+    e.preventDefault()
     const { name, email } = this.state
     const response = await addToMailchimp(email, { name: name })
     const { result, msg } = response
-    this.setState(() => ({ response: { result, msg }, name: '', email: '' }))
+    this.setState(() => ({ response: { result, msg }, name: "", email: "" }))
     console.log(this.state.response)
-
   }
   render() {
     const { name, email, response } = this.state
     return (
-      <div style={{ padding: "1rem" }} className="newsletter">
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "1rem" }}>
-          <h3 style={{ margin: "0" }}>Stay in touch</h3>
-          <IoMdMail style={{ color: "var(--primaryTheme)", fontSize: "2.5rem" }} />
+      <div className="row newsletter px-sm-4 py-4 mb-3 mr-sm-0 ml-sm-0">
+        <div className="col-sm">
+          <div className="d-flex justify-content-between align-items-center">
+            <h3 className="m-0">Newsletter</h3>
+            <IoMdMailOpen
+              style={{ color: "var(--primaryTheme)", fontSize: "2.5rem" }}
+            />
+          </div>
+          <div className="mt-4">
+            <p>
+              If you want to get my latest posts don't forget to subscribe 🙂
+            </p>
+          </div>
+          {response.msg !== undefined && (
+            <div style={{ alignItems: "center", marginBottom: "1rem" }}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: getSubscriptioPreferenceUrl(response.msg),
+                }}
+              />
+            </div>
+          )}
         </div>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "1rem" }}>
-          {response.msg !== undefined && <div>
-            <p dangerouslySetInnerHTML={{ __html: getSubscriptioPreferenceUrl(response.msg) }} />
-          </div>}
-        </div>
-        <div>
-          <form className="" onSubmit={this._handleSubmit}>
+        <div className="col-sm">
+          <form onSubmit={this._handleSubmit}>
             <div className="form-group">
-              <input className="form-control" placeholder="Name" onChange={this.onNameChange} type="text" value={name} />
+              <input
+                className="form-control"
+                placeholder="Name"
+                onChange={this.onNameChange}
+                type="text"
+                value={name}
+              />
             </div>
             <div className="form-group">
-              <input className="form-control" placeholder="Email" onChange={this.onEmailChange} type="email" value={email} />
+              <input
+                className="form-control"
+                placeholder="Email"
+                onChange={this.onEmailChange}
+                type="email"
+                value={email}
+              />
             </div>
             <div className="form-group">
-              <input className="btn btn-block" type="submit" value="Subscribe" />
+              <input
+                className="btn btn-block"
+                type="submit"
+                value="Subscribe"
+              />
             </div>
           </form>
         </div>
