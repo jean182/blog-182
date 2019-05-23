f87eaa325129f067010ff269ed0ac3b3197f1bc1
Jean Marco <jmarco@IT-CR-0678.local>
2019-05-23 17:53:38 -0600

    Added new bg for not found

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,35 +6,27 @@ import SEO from "../components/seo"
 import { GiUfo } from "react-icons/gi"
 import "../styles/void.css"
 
-function transform(offset) {
-  const cos = Math.cos(offset)
-  const sin = Math.sin(offset)
-  return {
-    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`,
-  }
-}
-
 class NotFoundPage extends React.Component {
-  state = { styleOne: {}, styleTwo: {} }
-
-  onMouseMove = event => {
-    this.setState({
-      styleOne: transform(-event.clientX / event.clientY),
-      styleTwo: transform(event.clientX / event.clientY),
-    })
-  }
-
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <div className="void-container" onMouseMove={this.onMouseMove}>
-          <div className="void void-container" style={this.state.styleOne} />
-          <div className="void void-container" style={this.state.styleTwo}>
+      <div className="void">
+        <Layout location={this.props.location} title={siteTitle}>
+          <SEO title="404: Not Found" />
+          <div>
             <h1
+              className="m-0"
+              style={{
+                textAlign: "center",
+                fontSize: "16rem",
+              }}
+            >
+              <GiUfo />
+            </h1>
+            <h1
+              className="m-0"
               style={{
                 fontFamily: `Montserrat, sans-serif`,
                 textAlign: "center",
@@ -43,6 +35,7 @@ class NotFoundPage extends React.Component {
               404
             </h1>
             <h3
+              className="mt-0"
               style={{
                 fontFamily: `Montserrat, sans-serif`,
                 fontWeight: "bold",
@@ -51,17 +44,9 @@ class NotFoundPage extends React.Component {
             >
               You've entered the void
             </h3>
-            <h1
-              style={{
-                textAlign: "center",
-                fontSize: "4rem",
-              }}
-            >
-              <GiUfo />
-            </h1>
           </div>
-        </div>
-      </Layout>
+        </Layout>
+      </div>
     )
   }
 }
