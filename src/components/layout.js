fadbd6386cc2ff09a30e1f93a5fb605aad52bdd5
Jean Aguilar <jean-marco-10@hotmail.com>
2019-12-25 18:32:52 -0600

    Test try catch statement on deploy (#29)
    * Try catch statement on deploy when is running in node and add translation logic for missing translations

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react"
-
+import { consoleWarning } from "../utils/helpers"
 import { rhythm } from "../utils/typography"
-import { isRunningInBrowser } from "../utils/helpers"
 import Header from "./header"
 import Footer from "./footer"
 
 function Layout(props) {
-  let findTheme =  "light"
-  if (isRunningInBrowser()) findTheme = JSON.parse(localStorage.getItem("theme")) || "light"
-  const [theme, setTheme] = useState(findTheme);
-  const [isOn, setIsOn] = useState(theme === "dark");
+  let findTheme = "light"
+  try {
+    findTheme = JSON.parse(localStorage.getItem("theme")) || "light"
+  } catch (error) {
+    consoleWarning(error)
+  }
+  const [theme, setTheme] = useState(findTheme)
+  const [isOn, setIsOn] = useState(theme === "dark")
 
   useEffect(() => {
-    if (isRunningInBrowser()) {
+    try {
       document.documentElement.setAttribute("data-theme", theme)
       localStorage.setItem("theme", JSON.stringify(theme))
       const metaThemeColor = document.querySelector("meta[name=theme-color]")
@@ -20,35 +23,37 @@ function Layout(props) {
         "content",
         theme === "light" ? "#356CB5" : "#f76c6c"
       )
+    } catch (error) {
+      consoleWarning(error)
     }
-  });
+  })
 
   function toggleTheme() {
-    setTheme(prevState => prevState === "light" ? "dark" : "light")
+    setTheme(prevState => (prevState === "light" ? "dark" : "light"))
     setIsOn(prevState => !prevState)
   }
 
   const { title, children, currentLanguage } = props
 
   return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-        className="app"
-      >
-        <Header
-          title={title}
-          theme={theme}
-          isOn={isOn}
-          toggleTheme={toggleTheme}
-        />
-        <main>{children}</main>
-        <Footer currentLanguage={currentLanguage} />
-      </div>
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+      className="app"
+    >
+      <Header
+        title={title}
+        theme={theme}
+        isOn={isOn}
+        toggleTheme={toggleTheme}
+      />
+      <main>{children}</main>
+      <Footer currentLanguage={currentLanguage} />
+    </div>
   )
 }
 
